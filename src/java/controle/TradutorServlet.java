6ca3ef388dfbb3760fc64705432fe3056bccfeb6
Paulo Campos <paulorcv@gmail.com>
2016-09-22 06:56:59 -0300

    Criação de servlet e página inicial

---
+++ src/java/controle/TradutorServlet.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controle;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author paulorcv
+ */
+@WebServlet("/traduzir")
+public class TradutorServlet extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+   
+            String palavra = request.getParameter("palavra");
+            String palavraTraduzida = "retorno tradução";
+            
+            request.setAttribute("palavraTraduzida", palavraTraduzida);
+            request.getRequestDispatcher("resultado.jsp").forward(request, response);
+            
+            
+        }
+    }
+
+ 
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+}
---
+++ web/resultado.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : resultado
+    Created on : 13/09/2016, 20:55:48
+    Author     : paulorcv
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Tradutor</title>
+    </head>
+    <body>
+        <h1>O resultado da palavra ${param.palavra} é ${palavraTraduzida}</h1>
+    </body>
+</html>
